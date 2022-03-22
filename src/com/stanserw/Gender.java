594d1830e34d57b47cb6db2d1a5efbb8c00d5725
Stanisław Serwin <stanserw@wp.pl>
2022-03-22 21:57:53 +0100

    Initial commit

---
+++ src/com/stanserw/Gender.java
@@ -0,0 +1,5 @@
+package com.stanserw;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/com/stanserw/Main.java
@@ -0,0 +1,90 @@
+package com.stanserw;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> people = getPeople();
+
+        // Imperative approach
+/*        List<Person> females = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getGender().equals(Gender.FEMALE)) {
+                females.add(person);
+            }
+        }
+        females.forEach(System.out::println);*/
+
+        // Declarative approach
+
+        // Filter
+        List<Person> females = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .collect(Collectors.toList());
+//        females.forEach(System.out::println);
+
+        // Sort
+        List<Person> sorted = people.stream()
+                .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender).reversed())
+                .collect(Collectors.toList());
+        sorted.forEach(System.out::println);
+
+        // All match
+        boolean allMatch = people.stream()
+                .allMatch(person -> person.getAge() > 5);
+//        System.out.println(allMatch);
+
+        // Any match
+        boolean anyMatch = people.stream()
+                .anyMatch(person -> person.getAge() > 8);
+//        System.out.println(anyMatch);
+
+        // None match
+        boolean noneMatch = people.stream()
+                .noneMatch(person -> person.getName().equals("Jemi Goa"));
+//        System.out.println(noneMatch);
+
+        // Max
+        people.stream()
+                .max(Comparator.comparing(Person::getAge));
+//                .ifPresent(System.out::println);
+
+        // Min
+        people.stream()
+                .min(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+
+        // Group
+/*        Map<Gender, List<Person>> groupByGender = people.stream()
+                .collect(Collectors.groupingBy(Person::getGender));
+
+        groupByGender.forEach((gender, people1) -> {
+            System.out.println(gender);
+            people1.forEach(System.out::println);
+            System.out.println();
+        });*/
+
+/*        Optional<Gender> oldestFemaleAge = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .max(Comparator.comparing(Person::getAge))
+                .map(Person::getGender);
+
+        oldestFemaleAge.ifPresent(System.out::println);*/
+
+    }
+
+    private static List<Person> getPeople() {
+
+        return List.of(
+                new Person("James Bond", 20, Gender.MALE),
+                new Person("Alina Smith", 33, Gender.FEMALE),
+                new Person("Helen White", 57, Gender.FEMALE),
+                new Person("Alex Boz", 14, Gender.MALE),
+                new Person("Alon Goa", 99, Gender.MALE),
+                new Person("Anna Cook", 7, Gender.FEMALE),
+                new Person("Zelda Brown", 120, Gender.FEMALE)
+        );
+    }
+}
